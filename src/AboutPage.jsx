6cbeee946c47f7e8ef5fb88f9342ead7002959c4
Sanaa <sanaa@sanaaphotography.com>
2026-06-24 21:51:24 -0500

    about me updates

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -98,7 +98,7 @@ function BioAndFacts() {
           <FadeIn delay={0.1}>
             <blockquote style={{ fontFamily:"'Libre Baskerville', serif", fontStyle:"italic",
               fontSize:"1.2rem", color:BISQUE, lineHeight:1.55, marginBottom:"2.75rem" }}>
-              If I'm not shooting weddings, you'll find me out in Austin, exploring the city in search for some good brisket, or at home cuddling my cat, Flan.
+              If I'm not shooting weddings, you'll find me out in Austin, shopping at local markets, in line at a food pop-up, or at home cuddling my cats, Flan and Rasu (not pictured, she's shy).
             </blockquote>
           </FadeIn>
 
